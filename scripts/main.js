6e08b3af7800ff97bb00c6443b311897fe2dfee9
Daniel Kua <danielkua888@live.com>
2024-06-21 04:41:16 -0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,58 @@
 let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-
 showSlides(slideIndex);
 
 function currentSlide(n) {
-    slideIndex = n;
-    showSlides(slideIndex);
+    showSlides(slideIndex = n - 1);
 }
 
 function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    let dots = document.querySelectorAll('.dot');
     if (n >= slides.length) slideIndex = 0;
     if (n < 0) slideIndex = slides.length - 1;
-
-    slides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-
-    slides[slideIndex].style.display = 'block';
+    slides.forEach(slide => slide.style.transform = `translateX(-${slideIndex * 100}%)`);
     dots.forEach(dot => dot.classList.remove('active'));
     dots[slideIndex].classList.add('active');
 }
 
-// Add smooth horizontal scroll event listener for navigation
-let startX = 0;
-let endX = 0;
-window.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].clientX;
-});
-window.addEventListener('touchend', (e) => {
-    endX = e.changedTouches[0].clientX;
-    if (startX > endX + 50) { // Adjust the value for better sensitivity
-        currentSlide(slideIndex + 1);
-    } else if (startX < endX - 50) {
-        currentSlide(slideIndex - 1);
-    }
-});
+document.querySelector('.hero-slider').addEventListener('touchstart', handleTouchStart, false);        
+document.querySelector('.hero-slider').addEventListener('touchmove', handleTouchMove, false);
 
-// Enable scrolling with mouse wheel or trackpad
-window.addEventListener('wheel', (e) => {
-    if (e.deltaY > 0) {
-        currentSlide(slideIndex + 1);
-    } else {
-        currentSlide(slideIndex - 1);
+let xDown = null;                                                        
+let yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+           evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
     }
-});
+
+    let xUp = evt.touches[0].clientX;                                    
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+                                                            
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/* most significant */
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            showSlides(slideIndex += 1);
+        } else {
+            /* right swipe */
+            showSlides(slideIndex -= 1);
+        }                       
+    } 
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
